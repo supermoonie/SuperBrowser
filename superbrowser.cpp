803d1fb1eb627b6c2d0c67d99a056ae997f86776
supermoonie <wangchao_moonie@163.com>
2018-09-21 18:20:32 +0800

    update

--- superbrowser.cpp
+++ superbrowser.cpp
@@ -4,6 +4,8 @@ SuperBrowser::SuperBrowser(QObject* parent): QObject(parent)
 {
     cookieJar = MemoryCookieJar::instance();
     webPage = WebPage::instance();
+    commandMap = new QMap<QString, FUN>;
+    commandMap->insert("navigate", &SuperBrowser::navigate);
     receiveThread = new ReceiveThread(NULL);
     connect(receiveThread, &ReceiveThread::received, this, &SuperBrowser::onCommandReceived);
     receiveThread->start();
@@ -11,12 +13,55 @@ SuperBrowser::SuperBrowser(QObject* parent): QObject(parent)
 
 SuperBrowser::~SuperBrowser()
 {
+    commandMap->clear();
+    delete commandMap;
     receiveThread->stop();
     receiveThread->deleteLater();
     cookieJar->deleteLater();
     webPage->deleteLater();
 }
 
+QJsonObject SuperBrowser::navigate(QJsonArray &parameters) {
+    webPage->action(QWebPage::Stop);
+    QString url = parameters.at(0).toObject().value("url").toString("about:blank");
+    webPage->currentFrame()->load(QUrl::fromUserInput(url));
+    QJsonObject json;
+    json.insert("code", 200);
+    return json;
+}
+
 void SuperBrowser::onCommandReceived(const QString &rawCommand) {
-    Terminal::instance()->cout(rawCommand, true);
+    QJsonParseError * parseError = new QJsonParseError;
+    QJsonDocument commandJsonDocument = QJsonDocument::fromJson(rawCommand.toUtf8(), parseError);
+    if(parseError->error != QJsonParseError::NoError)
+    {
+        QJsonObject errorJson;
+        errorJson.insert("code", 400);
+        errorJson.insert("desc", parseError->errorString());
+        errorJson.insert("data", QJsonValue::Null);
+        Terminal::instance()->cout(QString(QJsonDocument(errorJson).toJson()), true);
+        return;
+    }
+    QJsonObject commandJson = commandJsonDocument.object();
+    QString name = commandJson.value("name").toString();
+    if(name.isNull() || name.isEmpty()) {
+        QJsonObject errorJson;
+        errorJson.insert("code", 400);
+        errorJson.insert("desc", "name lost");
+        errorJson.insert("data", QJsonValue::Null);
+        Terminal::instance()->cout(QString(QJsonDocument(errorJson).toJson()), true);
+        return;
+    }
+    if(!commandMap->contains(name)) {
+        QJsonObject errorJson;
+        errorJson.insert("code", 404);
+        errorJson.insert("desc", name + " not support");
+        errorJson.insert("data", QJsonValue::Null);
+        Terminal::instance()->cout(QString(QJsonDocument(errorJson).toJson()), true);
+        return;
+    }
+    FUN fun = commandMap->value(name);
+    QJsonArray parameters = commandJson.value("parameters").toArray();
+    QJsonObject json = (this->*fun)(parameters);
+    Terminal::instance()->cout(QString(QJsonDocument(json).toJson()), true);
 }
--- superbrowser.h
+++ superbrowser.h
@@ -2,12 +2,19 @@
 #define SUPERBROWSER_H
 
 #include <QObject>
+#include <QJsonParseError>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QMap>
 #include "webpage.h"
 #include "memorycookiejar.h"
 #include "receivethread.h"
 
 class SuperBrowser: public QObject
 {
+    typedef QJsonObject(SuperBrowser::*FUN)(QJsonArray&);
+
 public:
     SuperBrowser(QObject* parent = NULL);
     ~SuperBrowser();
@@ -16,6 +23,9 @@ private:
     ReceiveThread* receiveThread;
     MemoryCookieJar* cookieJar;
     WebPage* webPage;
+    QMap<QString, FUN>* commandMap;
+
+    QJsonObject navigate(QJsonArray &parameters);
 
 private slots:
     void onCommandReceived(const QString &rawCommand);
